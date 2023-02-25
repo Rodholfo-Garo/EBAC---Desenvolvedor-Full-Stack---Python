aa28cbc1278a3b42e6bec46bdd888c248a8be680
RODHOLFO_EBAC <ro.garo87@gmail.com>
2023-02-25 05:37:00 -0300

    Envio Exxercicio jQuery

---
+++ Projetos -Dev. Full/08 Projeto - FormularioDeCadastro_jQuery/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('#edit_cpf').mask('000.000.000-00',{
+        placeholder:'Digite seu CPF'
+    })
+
+    $('#cep').mask('00000-000',{
+        placeholder:'Digite seu CEP'
+    })
+
+
+
+})
+
+
